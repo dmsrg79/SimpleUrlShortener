8f4d00ef58784819021421117badb017e2a7bae5
Dmitriy <46328070+dmsrg79@users.noreply.github.com>
2020-04-29 23:13:16 +0500

    Add files via upload

---
+++ short.php
@@ -0,0 +1,19 @@
+Select * from urls where short_url<?php
+
+try {
+  $pdo = new PDO('mysql:host=localhost;dbname=dima', 'root','password');
+} catch (PDOException $e) {
+  print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+$hash = $_GET['short_code'];
+if(!empty($hash)){
+ $stmt = $pdo->prepare('Select * from urls where short_code=:short_code');
+ $stmt->bindParam(":short_code",$hash);
+ $stmt->execute();
+ $res = $stmt->fetch(PDO::FETCH_OBJ);
+ header("Location: $res->full_url");
+}
+
+?>
